72e30701be2bb215da58c74b1327c7d5e8936195
Kristiyan Rulev <calabangata@abv.bg>
2023-04-20 12:59:42 +0300

    Creating controller for leave request and view model

---
+++ LeaveManagmentSystem.Web/Models/LeaveRequestCreateVM.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagmentSystem.Web.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagmentSystem.Web.Models
+{
+    public class LeaveRequestCreateVM
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int LeaveTypeId { get; set; }
+        public SelectList LeaveTypes { get; set; }
+        public string RequestComments { get; set; }
+    }
+}
